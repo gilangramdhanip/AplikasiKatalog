dde1dc979ed33205908074e10cee4b99a618842a
Gilang Ramdhani Putra <gramdhani66@gmail.com>
2021-09-12 14:36:21 +0800

    Initial Commit

---
+++ AplikasiKatalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AplikasiKatalog
+//
+//  Created by Gilang Ramdhani Putra on 12/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
